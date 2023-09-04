e78dd96ebbc7afebfe52350dd6d50ecc99b710b9
Andres Serrano <a.serranoc@uniandes.edu.co>
2023-09-04 17:46:03 -0500

    UML

--- Taller2/Taller2Parte1/src/mundo/Pedido.java
+++ Taller2/Taller2Parte1/src/mundo/Pedido.java
@@ -11,66 +11,64 @@ public class Pedido {
 	private String nombreCliente;
 	private String direccionCliente;
 	private ArrayList<Producto> itemsPedido;
-	public Pedido(String pNombreCliente, String pDireccionCliente)
-	{
-		nombreCliente=pNombreCliente;
-		direccionCliente=pDireccionCliente;
-		itemsPedido=new ArrayList<Producto>();
-		idPedido=numeroPedidos+1;
+
+	public Pedido(String pNombreCliente, String pDireccionCliente) {
+		nombreCliente = pNombreCliente;
+		direccionCliente = pDireccionCliente;
+		itemsPedido = new ArrayList<Producto>();
+		idPedido = numeroPedidos + 1;
 		numeroPedidos++;
-		
+
 	}
-	public int getIdPedido()
-	{
+
+	public int getIdPedido() {
 		return idPedido;
 	}
-	public void agregarProducto(Producto nuevoItem)
-	{
+
+	public void agregarProducto(Producto nuevoItem) {
 		itemsPedido.add(nuevoItem);
 	}
-	private int getPrecioNetoPedido()
-	{
-		int precio=0;
-		for (int i=0; i<itemsPedido.size();i++)
-		{
-			precio=precio+itemsPedido.get(i).getPrecio();
+
+	private int getPrecioNetoPedido() {
+		int precio = 0;
+		for (int i = 0; i < itemsPedido.size(); i++) {
+			precio = precio + itemsPedido.get(i).getPrecio();
 		}
 		return precio;
 	}
-	private int getPrecioTotalPedido()
-	{
-		return getPrecioNetoPedido()+getPrecioIVAPedido();
+
+	public int getPrecioTotalPedido() {
+		return getPrecioNetoPedido() + getPrecioIVAPedido();
 	}
-	private int getPrecioIVAPedido()
-	{
-		return (int) Math.round(getPrecioNetoPedido()*0.19);
+
+	private int getPrecioIVAPedido() {
+		return (int) Math.round(getPrecioNetoPedido() * 0.19);
 	}
-	public String generarTextoFactura()
-	{
-		String texto="Nombre cliente: "+nombreCliente+"\n"+"Dirección cliente: "+direccionCliente+"\n";
-		texto=texto+"Items en el pedido"+"\n"+"------------------";
-		for (int i=0;i<itemsPedido.size();i++)
-		{
-			texto=texto+"\n"+itemsPedido.get(i).generarTextoFactura();
+
+	public String generarTextoFactura() {
+		String texto = "Nombre cliente: " + nombreCliente + "\n" + "Dirección cliente: " + direccionCliente + "\n";
+		texto = texto + "Items en el pedido" + "\n" + "------------------";
+		for (int i = 0; i < itemsPedido.size(); i++) {
+			texto = texto + "\n" + itemsPedido.get(i).generarTextoFactura();
 		}
-		texto=texto+"\n"+"Precio neto: $"+getPrecioNetoPedido()+"\n"+"IVA: $"+getPrecioIVAPedido()+"\n"+"Precio total: $"+getPrecioTotalPedido();
-		
+		texto = texto + "\n" + "Precio neto: $" + getPrecioNetoPedido() + "\n" + "IVA: $" + getPrecioIVAPedido() + "\n"
+				+ "Precio total: $" + getPrecioTotalPedido();
+
 		return texto;
 	}
-	public void guardarFactura(File archivo)
-	{
+
+	public void guardarFactura(File archivo) {
 		try {
-		      FileWriter escritor = new FileWriter(archivo.getPath());
-		      escritor.write(generarTextoFactura());
-		      escritor.close();
-		    } catch (IOException e) {
-		      e.printStackTrace();
-		    }
+			FileWriter escritor = new FileWriter(archivo.getPath());
+			escritor.write(generarTextoFactura());
+			escritor.close();
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
 	}
-	public ArrayList<Producto> getItemsPedido()
-	{
+
+	public ArrayList<Producto> getItemsPedido() {
 		return itemsPedido;
 	}
-	
 
 }
